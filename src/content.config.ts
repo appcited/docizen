c4a12292655c608633e2803991d52c1abd65a167
Marek Brieger <m.brieger@ra-micro.de>
2025-02-11 23:35:03 +0100

    dev

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,7 +6,7 @@ const config = await getConfig();
 
 const collections = config.content.reduce((acc: { [key: string]: CollectionConfig<any> }, content) => {
   acc[content.name] = defineCollection({
-    loader: glob({ pattern: `**/*.md[x]`, base: `file://${import.meta.env.PUBLIC_USER_ROOT}` }),
+    loader: glob({ pattern: `**/*.(md|mdx)`, base: `file://${import.meta.env.PUBLIC_USER_ROOT}` }),
     schema: z.object({
       layout: z.string().default("./src/layouts/Layout.astro"),
     }),
