a045305fdd57c16b64ea7014fe67a10cd573fd48
Marek Brieger <m.brieger@ra-micro.de>
2025-02-11 23:30:00 +0100

    dev

--- src/config.ts
+++ src/config.ts
@@ -3,12 +3,22 @@ import { mergeDeepLeft } from "ramda"
 
 const Config = z.object({
   name: z.string(),
+  content: z.array(z.object({
+    name: z.string(),
+    basePath: z.string(),
+  }))
 });
 
 export type Config = z.infer<typeof Config>;
 
 export const defaultConfig = () => ({
   name: "Docizen",
+  content: [
+    {
+      name: "root",
+      basePath: ".",
+    },
+  ],
 } satisfies Partial<Config>);
 
 export const getConfig = async () => {
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,16 +1,19 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z, type CollectionConfig } from "astro:content";
 import { glob } from 'astro/loaders';
-import { env } from 'process';
+import { getConfig } from "./config";
 
-const contentBase = `file://${env.PUBLIC_USER_ROOT}`
+const config = await getConfig();
 
-const root = defineCollection({
-  loader: glob({ pattern: `**/*.md[x]`, base: contentBase }),
-  schema: z.object({
-    layout: z.string().default("./src/layouts/Layout.astro"),
-  }),
-});
+const collections = config.content.reduce((acc: { [key: string]: CollectionConfig<any> }, content) => {
+  acc[content.name] = defineCollection({
+    loader: glob({ pattern: `**/*.md[x]`, base: `file://${import.meta.env.PUBLIC_USER_ROOT}` }),
+    schema: z.object({
+      layout: z.string().default("./src/layouts/Layout.astro"),
+    }),
+  });
+  return acc;
+}, {});
 
-export const collections = {
-  root,
-};
+export {
+  collections
+}
