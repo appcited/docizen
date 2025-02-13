af10704a6a7db89aaab8f21c45f5a87f997e1d19
Marek Brieger <m.brieger@ra-micro.de>
2025-02-13 01:43:00 +0100

    dev

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 import { mergeDeepLeft } from "ramda"
 
 const Config = z.object({
+  _docizenRoot: z.string().url(),
+  _userRoot: z.string().url(),
   name: z.string(),
   icon: z.string().url(),
   content: z.array(z.object({
@@ -14,6 +16,8 @@ const Config = z.object({
 export type Config = z.infer<typeof Config>;
 
 export const defaultConfig = () => ({
+  _docizenRoot: import.meta.env.PUBLIC_DOCIZEN_ROOT,
+  _userRoot: import.meta.env.PUBLIC_USER_ROOT,
   name: "Docizen",
   icon: `${import.meta.env.PUBLIC_DOCIZEN_ROOT}\\src\\assets\\icon.svg`,
   content: [
@@ -26,7 +30,7 @@ export const defaultConfig = () => ({
 } satisfies Partial<Config>);
 
 export const getConfig = async () => {
-  const userConfig = (await import(/* @vite-ignore */ `file://${import.meta.env.PUBLIC_USER_ROOT}\\.docizen\\config.mjs`)).default;
-  const mergedConfig = mergeDeepLeft(userConfig, defaultConfig());
+  const userConfigFile = await import(`file://${import.meta.env.PUBLIC_USER_ROOT}\\.docizen\\config.mjs`)
+  const mergedConfig = mergeDeepLeft(userConfigFile.default, defaultConfig());
   return Config.parse(mergedConfig);
 };
